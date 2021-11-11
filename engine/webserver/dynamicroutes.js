a43cfb60d6dbc310d39cdec0d44ef80bd5ce6459
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-11 13:31:39 +0000

    dynamic routes loader

--- engine/webserver/dynamicroutes.js
+++ engine/webserver/dynamicroutes.js
@@ -1,4 +1,14 @@
+const { dynamicroutes } = require('/engine/settings.json');
+function routesloader(app, dynamicroutes) {
+    let arraynum = 0;
+    app.get(`${dynamicroutes[arraynum]}`, (request, response) => {
+        response.render(dynamicroutes[arraynum]);
+    });
+    let oldarraynum = arraynum
+    arraynum = oldarraynum + 1;
+}
 function webserverdynamicroutes(app) {
-
+    dynamicroutes.array.forEach(routesloader);
+    console.log('called the dynamic routes loader')
 }
 module.exports = webserverdynamicroutes;
