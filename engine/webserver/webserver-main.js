2ebf413e0177d2896bddd37b2afd04df2dc27a5c
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-11 13:09:00 +0000

    added console.log statements

--- engine/webserver/webserver-main.js
+++ engine/webserver/webserver-main.js
@@ -3,12 +3,17 @@ function webserversections(express, app) {
     const webserverroutes = require('/engine/webserver/routes.js')
     const webserverdynamicroutes = require('/engine/webserver/dynamicroutes.js')
     webserverconfig(express, app)
+    console.log('called the webserver config')
     webserverdynamicroutes(app)
+    console.log('called the dynamic routes')
     webserverroutes(app)
+    console.log('called the standard routes')
+    console.log('finished calling webserver sections')
 }
 function webservercall() {
     const express = require('express');
     const app = express()
     webserversections(express, app)
+    console.log('called the webserver')
 }
 module.exports = webservercall;
