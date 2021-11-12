b58140d9505f0918b2f582807068049c5134b390
devramsean0 <81807361+devramsean0@users.noreply.github.com>
2021-11-12 08:26:33 +0000

    added / route

--- engine/webserver/config.js
+++ engine/webserver/config.js
@@ -2,5 +2,8 @@ function webserverconfig(express, app) {
     app.set("view engine", "html");
     app.set("views", "/game");
     console.log('configured the webserver')
+    app.get('/', (request, response) => {
+        response.render('index);
+    });
 }
 module.exports = webserverconfig;
